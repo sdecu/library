be1822526b1e7b6a5c4b29f379d133f7a65491b5
Chris <sdecuir12@gmail.com>
2024-03-24 23:35:24 -0500

    add function to add book to library;

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+const open = document.querySelector('#dialog');
+const modal = document.querySelector('.modal');
+open.addEventListener("click", () => {
+modal.showModal();
+});
+
+const button = document.querySelector('button');
+button.addEventListener('click', addBookToLibrary);
+
 const myLibrary = [];
 
-function Book(author, title, pages) {
+function Book(author, title, pages, read) {
   this.author = author;
   this.title = title;
   this.pages = pages;
-  this.read = false;
+  this.read = read;
 }
 
 function addBookToLibrary() {
   // do stuff here 
+    const author = document.querySelector('#author').value;
+    const title = document.querySelector('#title').value;
+    const pages = document.querySelector('#pages').value;
+    const readStatus = document.querySelector('input[name="readStatus"]:checked').value === 'true';
+    const newBook = new Book(author, title, pages, readStatus);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
 }
-
-const open = document.querySelector('#dialog');
-const modal = document.querySelector('.modal');
-
-open.addEventListener("click", () => {
-modal.showModal();
-});
